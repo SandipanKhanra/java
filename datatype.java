1ef9d804c5cd7ba5bae59ae85487e4c2336ad6cc
Sandipan Khanra <46983139+SandipanKhanra@users.noreply.github.com>
2020-04-18 14:15:44 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,19 @@
+//data types as it name suggests it specifies which types of data a variable can store
+//and what types of operation can be performed on them
+// Like int- to store integer values
+//char- for characters,double,float- for floating points
+//Others are short, boolean which stores either true or false
+//These are primitive datatypes
+
+
+public class datatype {
+
+	public static void main(String[] args) {
+		int n=4;
+		char c='s';
+		double d=4.5;
+		System.out.println(n);
+		System.out.println(c);
+		System.out.println(d);	
+	}
+}
