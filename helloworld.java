e8ff1c4ff8d145dca69898d765618bb92093a678
Sandipan Khanra <46983139+SandipanKhanra@users.noreply.github.com>
2020-04-20 01:39:42 +0530

    Update and rename helloworld to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 //A simple program to print something in java
-//File name: test.java
+//File name: helloworld.java
 
-public class test {
+public class helloworld{
 
 	public static void main(String[] args) {
 		System.out.println("This is the first program in java,Hello world");		
