0dbafe066aa3054e7ad8bce7857695c8cbe6a1f7
Sandipan Khanra <46983139+SandipanKhanra@users.noreply.github.com>
2020-04-20 01:38:15 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,18 @@
+//This program takes input from user
+
+import java.util.Scanner;
+
+public class input{
+
+	public static void main(String[] args) {
+		int n;
+		double d;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter integer,double  value:");
+		n=sc.nextInt();
+		d=sc.nextDouble();
+		System.out.println(n);
+		System.out.println(d);
+	}
+
+}
