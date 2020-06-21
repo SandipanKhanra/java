71cedaededd7172520daf1a9276b0fcafbfdc58a
Sandipan Khanra <46983139+SandipanKhanra@users.noreply.github.com>
2020-06-21 20:17:51 +0530

    Create MainClass.java

---
+++ polymorphism/MainClass.java
@@ -0,0 +1,17 @@
+ackage Polymorphism;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Pet p =d;
+        Animal a =d;
+
+        d.walk();
+        p.walk();
+        /**
+         * The output here will be Dog is walking in both cases
+         * This is because each time walk() from the Dog class is called
+         * but we can't know during compile time which method is going to call
+         * This is also known as Dynamic
+         */
+    }
